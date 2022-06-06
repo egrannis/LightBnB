38c1b12280d149db8828abbcee947ed4cd36d45f
Emma Grannis <grannis.emma@gmail.com>
2022-06-06 04:54:04 +0000

    Implement getAllProperties and addProperty functions in database.js

--- LightBnb_WebApp-master/server/database.js
+++ LightBnb_WebApp-master/server/database.js
@@ -18,15 +18,17 @@ const pool = new Pool({
  */
 
 const getUserWithEmail = (email) => {
-  return pool.query(`SELECT * from users WHERE users.email = $1`, [email])
+  return pool.query(`SELECT * from users WHERE users.email = $1;`, [email])
     .then((result) => {
-      console.log(result.rows[0]);
+      if (result.rows.length === 0) {
+        return null;
+      }
       return result.rows[0];
     })
     .catch((err) => {
-      return null;
+      console.log(err.message);
     });
-}
+};
 
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,17 +37,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-
+// return a promise with the user object for the specified user id, return null if user wasnt found in DB
 const getUserWithId = (id) => {
-  return pool.query(`SELECT * from users WHERE users.id = $1`, [id])
+  return pool.query(`SELECT * FROM users WHERE users.id = $1;`, [id])
     .then((result) => {
-      console.log(result.rows[0]);
+      if (result.rows.length === 0) {
+        return null;
+      }
       return result.rows[0];
     })
     .catch((err) => {
-      return null;
+      console.log(err.message);
     });
-}
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -56,15 +60,15 @@ exports.getUserWithId = getUserWithId;
  */
 
 const addUser = (user) => {
-  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
     .then((result) => {
       console.log(result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
-      return null;
+      console.log(err.message);
     });
-}
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -85,20 +89,20 @@ const getAllReservations = (guest_id, limit = 10) => {
     WHERE reservations.guest_id = $1
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
-    LIMIT $2`, 
+    LIMIT $2`,
     [guest_id, limit])
 
     .then((result) => {
-      // console.log(result.rows);
+      if (result.rows.length === 0) {
+        return null;
+      }
       return result.rows;
     })
     .catch((err) => {
-      console.log('Emma you made a mistake', err)
-      return null;
+      console.log(err.message);
     });
-}
-// POSSIBLE BUG - ON WEBPAGE IT DISPLAYS INCORRECT DATES. EG WHEN I DO A SQL QUERY IT SHOWS JAN 10, 2015 FOR ALIVE STONE
-// BUT IN THE WEBPAGE IT SHOWS JAN 9 2015
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -110,29 +114,108 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = (options, limit = 10) => {
+const getAllProperties = function(options, limit = 10) {
+  const values = [limit];
+  const whereClause = [];
+
+  //Set up the values array for the query and the conditions for the where clause based on what the user entered
+  let count = 2;
+  for (let key in options) {
+    if (options[key] !== '') {
+      //city
+      if (key === 'city') {
+        values.push(options[key]);
+        whereClause.push('properties.city = $' + count);
+      }
+      //minimum price per night
+      if (key === 'minimum_price_per_night') {
+        values.push(options[key] * 100);
+        whereClause.push('properties.cost_per_night >= $' + count);
+      }
+      //maximum price per night
+      if (key === 'maximum_price_per_night') {
+        values.push(options[key] * 100);
+        whereClause.push('properties.cost_per_night <= $' + count);
+      }
+      //minimum rating
+      if (key === 'minimum_rating') {
+        values.push(options[key]);
+        whereClause.push('rating >= $' + count);
+      }
+      count += 1;
+    }
+  }
+  //if there are user specified parameters, concatenate them into a string, otherwise return blank
+  const whereString = whereClause.length > 0 ? 'WHERE ' + whereClause.join(' AND ') + ' ' : '';
+
+  const queryString = `
+  SELECT DISTINCT properties.*, AVG(rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  ` + whereString +
+    `GROUP BY properties.id      
+    LIMIT $1;     
+  `;
+
+  //return array of objects containing properties that satisfy the query string
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, values)
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
     });
 };
-
 exports.getAllProperties = getAllProperties;
+
 
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+
+// Property
+// {
+//   owner_id: int,
+//   title: string,
+//   description: string,
+//   thumbnail_photo_url: string,
+//   cover_photo_url: string,
+//   cost_per_night: string,
+//   street: string,
+//   city: string,
+//   province: string,
+//   post_code: string,
+//   country: string,
+//   parking_spaces: int,
+//   number_of_bathrooms: int,
+//   number_of_bedrooms: int;
+// }
+
+const addProperty = function(property) {
+  const queryString = `
+  INSERT INTO properties (` + Object.keys(property) + `) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;;
+`;
+  //put the property variables into an array for the query
+  const values = Object.values(property);
+
+  //Replace blank integer values in the query parameter array if any happen to exist
+  if (values[2] === '') values[2] = '0';
+  if (values[3] === '') values[3] = '0';
+  if (values[4] === '') values[4] = '0';
+  if (values[5] === '') values[5] = '0';
+
+  //Create and return the new property
+  return pool
+    .query(queryString, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 exports.addProperty = addProperty;
